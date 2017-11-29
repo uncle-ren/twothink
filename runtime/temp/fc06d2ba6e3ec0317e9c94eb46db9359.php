4368c6ae002da19f2eabb58880568b27ba25a438
uncle-ren <1196129743@qq.com>
2017-11-29 11:59:14 +0800

    day3

---
+++ runtime/temp/fc06d2ba6e3ec0317e9c94eb46db9359.php
@@ -0,0 +1,135 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:63:"D:\jdk\public/../application/home/view/default/user\online.html";i:1511884837;}*/ ?>
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
+    <title>在线报修</title>
+
+    <!-- Bootstrap -->
+    <link href="../../index/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../index/css/style.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="//cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="//cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style>
+        .main{margin-bottom: 60px;}
+        .indexLabel{padding: 10px 0; margin: 10px 0 0; color: #fff;}
+    </style>
+</head>
+<body>
+<div class="main">
+    <!--导航部分-->
+    <nav class="navbar navbar-default navbar-fixed-bottom">
+        <div class="container-fluid text-center">
+            <div class="col-xs-3">
+                <p class="navbar-text"><a href="index.html" class="navbar-link">首页</a></p>
+            </div>
+            <div class="col-xs-3">
+                <p class="navbar-text"><a href="#" class="navbar-link">服务</a></p>
+            </div>
+            <div class="col-xs-3">
+                <p class="navbar-text"><a href="#" class="navbar-link">发现</a></p>
+            </div>
+            <div class="col-xs-3">
+                <p class="navbar-text"><a href="#" class="navbar-link">我的</a></p>
+            </div>
+        </div>
+    </nav>
+    <!--导航结束-->
+
+    <div class="span12">
+        <form class="login-form" action="" method="post">
+            <div class="control-group">
+                <label class="control-label" for="inputEmail">用户名</label>
+                <div class="controls">
+                    <input type="text" id="inputEmail" class="form-control" name="name" placeholder="请输入用户名"  ajaxurl="/member/checkUserNameUnique.html" errormsg="请填写1-16位用户名" nullmsg="请填写用户名" datatype="*1-16" value="" >
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label" for="inputPassword">密码</label>
+                <div class="controls">
+                    <input type="password" id="inputPassword"  placeholder="请输入密码"  errormsg="密码为6-20位" nullmsg="请填写密码" datatype="*6-20" class="form-control" name="tel">
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label" for="inputPassword">验证码</label>
+                <div class="controls">
+                    <input type="text" id="inputPassword" class="form-control" placeholder="请输入验证码"  errormsg="请填写5位验证码" nullmsg="请填写验证码" datatype="*5-5" name="verify">
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label"></label>
+                <div class="controls verifyimg">
+                    <?php echo captcha_img(); ?>
+                </div>
+                <div class="controls Validform_checktip text-warning"></div>
+            </div>
+            <div class="control-group">
+                <div class="controls">
+                    <label class="checkbox">
+                        <input type="checkbox" name="remember"> 自动登陆
+                    </label>
+                    <button class="btn btn-primary onlineBtn">登录</button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+</div>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="../../index/jquery-1.11.2.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="../../index/bootstrap/js/bootstrap.min.js"></script>
+
+
+<script type="text/javascript" >
+
+    $(document)
+        .ajaxStart(function(){
+            $("button:submit").addClass("log-in").attr("disabled", true);
+        })
+        .ajaxStop(function(){
+            $("button:submit").removeClass("log-in").attr("disabled", false);
+        });
+
+
+    $("form").submit(function(){
+        var self = $(this);
+        $.post(self.attr("action"), self.serialize(), success, "json");
+        return false;
+
+        function success(data){
+            if(data.code){
+                window.location.href = data.url;
+            } else {
+                self.find(".Validform_checktip").text(data.msg);
+                //刷新验证码
+                $(".verifyimg img").click();
+            }
+        }
+    });
+
+    $(function(){
+        //刷新验证码
+        var verifyimg = $(".verifyimg img").attr("src");
+        $(".verifyimg img").click(function(){
+            if( verifyimg.indexOf('?')>0){
+                $(".verifyimg img").attr("src", verifyimg+'&random='+Math.random());
+            }else{
+                $(".verifyimg img").attr("src", verifyimg.replace(/\?.*$/,'')+'?'+Math.random());
+            }
+        });
+    });
+</script>
+
+
+
+</body>
+</html>
